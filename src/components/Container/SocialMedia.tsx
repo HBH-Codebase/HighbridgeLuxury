6881fba3914b2d8b113aa501068160a658573a80
codedjay672 <emmajoel6456@gmail.com>
2024-11-28 12:24:34 +0100

    fix social media links

--- src/components/Container/SocialMedia.tsx
+++ src/components/Container/SocialMedia.tsx
@@ -1,24 +1,27 @@
 import { FaTwitterSquare } from "react-icons/fa";
-import { MdFacebook } from "react-icons/md";
+import { MdFacebook, MdWhatsapp } from "react-icons/md";
 import { SiInstagram } from "react-icons/si";
 import { Link } from "react-router-dom";
 
 const SocialMedia = () => {
   return (
     <div className="flex justify-start items-center gap-3 mt-2">
-      <Link to="">
+      <Link
+        to="https://web.facebook.com/profile.php?id=61563519050066"
+        target="blank"
+      >
         <MdFacebook
           size={25}
           className="text-gray-400 scale-110 transition-colors"
         />
       </Link>
-      <Link to="">
-        <SiInstagram
-          size={20}
+      <Link to="https://wa.link/276dn2" target="blank">
+        <MdWhatsapp
+          size={22}
           className="text-gray-400 scale-110 transition-colors"
         />
       </Link>
-      <Link to="">
+      <Link to="https://x.com/hb_luxuryltd" target="blank">
         <FaTwitterSquare
           size={20}
           className="text-gray-400 scale-110 transition-colors"
